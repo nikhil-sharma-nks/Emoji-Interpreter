e2aecd28d8779ddc69c52629f75525780dfaf737
Nikhil Sharma <nikhil.harsh.sharma@gmail.com>
2021-09-05 14:45:37 +0530

    feat: emojis entered shows meaning, button to clear input added

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
@@ -68,7 +67,4 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 
 ### `npm run build` fails to minify
 
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
-=======
-# Mark-08-Emoji-Interpreter
->>>>>>> ce848bae899834d7c8562b67cb3a69a4cd957633
+# This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
--- src/App.js
+++ src/App.js
@@ -1,9 +1,55 @@
 import "./App.css";
+import { useState } from "react";
+import emojisData from "./emojiData";
 
 function App() {
+  const [emoji, setEmoji] = useState("");
+  const [meaning, setMeaning] = useState(
+    "Your Interpretation of emoji will appear here!"
+  );
+  function inputOnChangeHandler(event) {
+    const inputValue = event.target.value.trim();
+    // const inputEmoji = inputValue[inputValue.length - 2];
+    setEmoji(inputValue);
+    console.log(emoji);
+    const emojiMeaning = emojisData[inputValue];
+    if (emojiMeaning) {
+      setMeaning(emojiMeaning);
+      console.log(emojiMeaning);
+    } else {
+      setMeaning(
+        "Either we don't have this emoji in our database or what you entered is not an emoji. Please input a single emoji at a time"
+      );
+    }
+  }
+
+  function handleClearOnClick() {
+    setEmoji("");
+    setMeaning("Your Interpretation of emoji will appear here!");
+  }
+
   return (
     <div className="App">
-      <div>Welcome</div>
+      <div className="heading">
+        <h1>Emoji Interpreter</h1>
+      </div>
+      <div className="description">
+        There are thousands of emoji available, but I am sure sometimes you
+        don't know what exactly do the mean. So I have compiled thousands of
+        emoji and therir meaning to help you!
+      </div>
+      <div className="input">
+        <input
+          type="text"
+          placeholder="Search you emoji here"
+          value={emoji}
+          onChange={inputOnChangeHandler}
+        ></input>
+        <button className="clearButton" onClick={handleClearOnClick}>
+          Clear
+        </button>
+      </div>
+      <div className="meaning">{meaning}</div>
     </div>
   );
 }
